2ecf78c36db05d8feb851230f2c1de55b9723ed1
Piyush Thapa <piyushthapa74@gmail.com>
2016-07-18 20:08:10 +0545

    top nav added

--- imports/ui/app.jsx
+++ imports/ui/app.jsx
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import Sidebar from './Sidebar.jsx'
+
+import MenuBar from './menuBar.jsx'
 //import Task from './Task.jsx';
 
 // App component - represents the whole app
 export default class App extends Component {
   render() {
     return (
-      <div className="row">
-        <div className="component-div col-md-3">
-          <Sidebar />
+      <div>
+        <div className="row top-bar">
+            <MenuBar />
         </div>
-        <div className="col-md-6">asdasd</div>
-        <div className="col-md-3">asdasda</div>
+        <div className="row">
+          <div className="component-div col-md-3">
+            <Sidebar />
+          </div>
+          <div className="col-md-6">asdasd</div>
+          <div className="col-md-3">asdasda</div>
+        </div>
       </div>
+
     );
   }
 }
