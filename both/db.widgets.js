463114f79ec1b9ed9efeb50b3c293db3d6b6d78c
Piyush Thapa <piyushthapa74@gmail.com>
2016-07-21 18:18:43 +0545

    added widget find

--- both/db.widgets.js
+++ both/db.widgets.js
@@ -8,5 +8,16 @@ Meteor.methods({
     new ComponentModel().insertMany(compoenet)
 
     return widgetDb.insert(widget)
+  },
+  getWidget:() =>{
+    let parsedWidget=[]
+    let widgetData= widgetDb.find({}).fetch()
+    _.each(widgetData,(widget)=>{
+      parsedWidget.push(
+        {widget:widget, elements:new ComponentModel().getComponentOfWidget(widget.elements)}
+      )
+    })
+    console.log("widget,", widgetData)
+    return parsedWidget
   }
 });
